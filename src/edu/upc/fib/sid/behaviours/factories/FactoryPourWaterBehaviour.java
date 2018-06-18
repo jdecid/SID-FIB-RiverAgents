8a1079419c7781199cc8a8044d52f72d341d13d5
JosepRIvaille <josep.de.cid@gmail.com>
2018-06-18 19:05:47 +0200

    Refactor on Pour water

--- src/edu/upc/fib/sid/behaviours/factories/FactoryPourWaterBehaviour.java
+++ src/edu/upc/fib/sid/behaviours/factories/FactoryPourWaterBehaviour.java
@@ -1,40 +1,51 @@
 package edu.upc.fib.sid.behaviours.factories;
 
 import edu.upc.fib.sid.helpers.*;
+import edu.upc.fib.sid.models.WaterTank;
 import jade.core.behaviours.OneShotBehaviour;
 import jade.lang.acl.ACLMessage;
 import jade.util.Logger;
 
+import static edu.upc.fib.sid.helpers.LoggerUtils.log;
+import static edu.upc.fib.sid.helpers.ReflectionUtils.findAndInvokeMethod;
+
 public class FactoryPourWaterBehaviour extends OneShotBehaviour {
     private Logger logger = Logger.getMyLogger(this.getClass().getName());
+    private Boolean messageSent = false;
 
     @Override
     public void action() {
-        ACLMessage msg = new ACLMessage(ACLMessage.QUERY_IF);
-        msg.addReceiver(Globals.TreatmentPlantAID);
-        msg.setContent("Can I pour water?");
-        myAgent.send(msg);
+        if (!messageSent) {
+            ACLMessage msg = new ACLMessage(ACLMessage.QUERY_IF);
+            msg.addReceiver(Globals.TreatmentPlantAID);
+            msg.setContent("Can I pour water?");
+            myAgent.send(msg);
+            messageSent = true;
+            log(logger, Logger.INFO, "Factory queries to pour water through the sewage system");
+        }
 
-        LoggerUtils.log(logger, Logger.INFO, "Factory queries to pour water through the sewage system");
-
-        msg = myAgent.receive();
+        ACLMessage msg = myAgent.receive();
         if (msg != null) {
             String senderType = DFUtils.getTypeByAID(myAgent, msg.getSender());
             if (Constants.TREATMENT_PLANT.equals(senderType)) {
                 if (msg.getPerformative() == ACLMessage.CONFIRM) {
-                    if (Constants.TREATMENT_PLANT.equals(senderType)) {
-                        ACLMessage reply = msg.createReply();
-                        reply.setPerformative(ACLMessage.INFORM);
-                        reply.setContent("I pour waste water");
-                        myAgent.send(reply);
+                    ACLMessage reply = msg.createReply();
+                    reply.setPerformative(ACLMessage.INFORM);
+                    reply.setContent("I pour waste water");
+                    myAgent.send(reply);
 
-                        ReflectionUtils.findAndInvokeMethod(myAgent, "emptyTank");
-                        LoggerUtils.log(logger, Logger.INFO, "Factory pours water through the sewage system");
-                    }
+                    WaterTank waterTank = (WaterTank) findAndInvokeMethod(myAgent, "getWasteWaterTank");
+                    waterTank.empty();
+                    
+                    log(logger, Logger.INFO, "Factory pours water through the sewage system");
+                    return;
                 } else if (msg.getPerformative() == ACLMessage.DISCONFIRM) {
-                    LoggerUtils.log(logger, Logger.INFO, "Factory can't pour water through the sewage system");
+                    log(logger, Logger.INFO, "Factory can't pour water through the sewage system");
+                    messageSent = false;
                 }
             }
-        } else block();
+        }
+
+        block();
     }
 }
